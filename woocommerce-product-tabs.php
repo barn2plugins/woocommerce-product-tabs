2cf81a9d7eaea639d1b847dcf440911af3583eb6
Nilambar Sharma <nilambar@outlook.com>
2020-05-24 21:18:16 +0545

    Update pot file

--- woocommerce-product-tabs.php
+++ woocommerce-product-tabs.php
@@ -1,18 +1,5 @@
 <?php
-
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * Dashboard. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://www.nilambar.net
- * @since             1.0.0
- * @package           Woocommerce_Product_Tabs
- *
- * @wordpress-plugin
  * Plugin Name: WooCommerce Product Tabs
  * Plugin URI: https://www.nilambar.net/2019/05/woocommerce-product-tabs-wordpress-plugin.html
  * Description: Custom Product Tabs for WooCommerce.
@@ -25,25 +12,30 @@
  * Domain Path: /languages
  * WC requires at least: 3.6.0
  * WC tested up to: 4.3.0
+ *
+ * @package Woocommerce_Product_Tabs
  */
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
 /**
- * Check if WooCommerce is active
- **/
+ * Check if WooCommerce is active.
+ */
 if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 	return;
 }
-// Define
+
+// Define.
 define( 'WOOCOMMERCE_PRODUCT_TABS_NAME', 'Woocommerce Product Tabs' );
 define( 'WOOCOMMERCE_PRODUCT_TABS_SLUG', 'woocommerce-product-tabs' );
 define( 'WOOCOMMERCE_PRODUCT_TABS_BASENAME', basename( dirname( __FILE__ ) ) );
 define( 'WOOCOMMERCE_PRODUCT_TABS_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) );
 define( 'WOOCOMMERCE_PRODUCT_TABS_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
 define( 'WOOCOMMERCE_PRODUCT_TABS_POST_TYPE_TAB', 'woo_product_tab' );
+
 /**
  * The code that runs during plugin activation.
  */
@@ -69,16 +61,11 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/class-woocommerce-product-t
 /**
  * Begins execution of the plugin.
  *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
+ * @since 1.0.0
  */
 function run_woocommerce_product_tabs() {
-
 	$plugin = new Woocommerce_Product_Tabs();
 	$plugin->run();
-
 }
+
 run_woocommerce_product_tabs();
