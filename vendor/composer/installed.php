51e7f22b3b41cf1572f3955869aeebd6a5f19b76
Luigi Pulcini <luigi@barn2.com>
2023-07-20 18:22:32 +0200

    fixed composer dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpconcern/woocommerce-product-tabs',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e83af89e806314374e3c0be9a75069def6ad5f94',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ernilambar/wp-admin-notice' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '626e4685775bdd06085e7af76f9ae9380c68c9f0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ernilambar/wp-admin-notice',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ernilambar/wp-welcome' => array(
+            'pretty_version' => '1.0.5',
+            'version' => '1.0.5.0',
+            'reference' => 'ff1371a36e335ac94ff9acc5285325ae1760da40',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ernilambar/wp-welcome',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpconcern/woocommerce-product-tabs' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e83af89e806314374e3c0be9a75069def6ad5f94',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
