1e11c2569f0f7b901516bf054644f2c30eabf515
amirition <amirition@outlook.com>
2023-12-02 16:24:06 +0100

    added main plugin classes

---
+++ src/Plugin.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Barn2\Plugin\WC_Product_Tabs_Free;
+
+use Barn2\Plugin\WC_Product_Tabs_Free\Dependencies\Lib\Plugin\Simple_Plugin;
+use Barn2\Plugin\WC_Product_Tabs_Free\Dependencies\Lib\Registerable;
+use Barn2\Plugin\WC_Product_Tabs_Free\Dependencies\Lib\Service;
+use Barn2\Plugin\WC_Product_Tabs_Free\Dependencies\Lib\Service_Provider;
+use Barn2\Plugin\WC_Product_Tabs_Free\Dependencies\Lib\Util;
+use Barn2\Plugin\WC_Product_Tabs_Free\Admin\Wizard\Setup_Wizard;
+
+/**
+ * The main plugin class.
+ *
+ * @package   Barn2\woocommerce-product-tabs
+ * @author    Barn2 Plugins <support@barn2.com>
+ * @license   GPL-3.0
+ * @copyright Barn2 Media Ltd
+ */
+class Plugin extends Simple_Plugin implements Registerable, Service_Provider {
+
+	const NAME    = 'WooCommerce Product Tabs Free';
+	const ITEM_ID = 559044;
+
+	/**
+	 * Services array.
+	 *
+	 * @var array $services
+	 */
+	private $services;
+
+	/**
+	 * Constructs and initializes the WooCommerce Product Tabs plugin instance.
+	 *
+	 * @param string $file    The main plugin __FILE__
+	 * @param string $version The current plugin version
+	 */
+	public function __construct( $file = null, $version = '1.0' ) {
+		parent::__construct(
+			[
+				'id'                 => self::ITEM_ID,
+				'name'               => self::NAME,
+				'version'            => $version,
+				'file'               => $file,
+				'settings_path'      => 'admin.php?page=wc-product-tabs',
+				'documentation_path' => 'kb/product-tabs-free-documentation/?utm_source=settings&utm_medium=settings&utm_campaign=settingsinline&utm_content=dlw-settings'
+			]
+		);
+
+		$this->add_service( 'plugin_setup', new Plugin_Setup( $this->get_file(), $this ), true );
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function register() {
+		parent::register();
+
+		add_action( 'plugins_loaded', [ $this, 'add_services' ] );
+
+		add_action( 'init', [ $this, 'register_services' ] );
+		add_action( 'init', [ $this, 'load_textdomain' ], 5 );
+	}
+
+	/**
+	 * Load the plugin.
+	 */
+	public function maybe_load_plugin() {
+		// Don't load plugin if Pro version active
+		if ( function_exists( '\Barn2\Plugin\WC_Product_Tabs_Pro\wta' ) ) {
+			return;
+		}
+	}
+
+	public function add_services() {
+		$this->add_service( 'wizard', new Setup_Wizard( $this ) );
+		$this->add_service( 'post_type', new Post_Type() );
+		$this->add_service( 'scripts', new Frontend_Scripts( $this ) );
+		$this->add_service( 'review_notice', new Review_Notice( $this ) );
+
+		// Admin only services
+		if ( Util::is_admin() ) {
+			$this->add_service( 'admin', new Admin\Admin_Controller( $this ) );
+		}
+
+	}
+
+	/**
+	 * Load the textdomain.
+	 */
+	public function load_textdomain() {
+		load_plugin_textdomain( 'woocommerce-product-tabs', false, $this->get_slug() . '/languages' );
+	}
+
+}
--- woocommerce-product-tabs.php
+++ woocommerce-product-tabs.php
@@ -1,9 +1,15 @@
 <?php
 /**
+ * @package   Barn2\woocommerce-product-tabs
+ * @author    Barn2 Plugins <support@barn2.com>
+ * @license   GPL-3.0
+ * @copyright Barn2 Media Ltd
+ *
+ * @wordpress-plugin
  * Plugin Name: WooCommerce Product Tabs (Free)
  * Plugin URI: https://barn2.com/wordpress-plugins/woocommerce-product-tabs/
- * Description: Boost your product pages by adding custom tabs containing extra information. Add an unlimited number of extra tabs for all products or specific categories.
- * Version: 2.0.24
+ * Description: Boost your product pages by adding custom tabs containing extra information.
+ * Version: 2.1.0
  * Author: Barn2 Plugins
  * Author URI: https://barn2.com
  * License: GPL-2.0+
@@ -14,93 +20,36 @@
  * Requires at least: 6.0
  * Tested up to: 6.4
  * WC requires at least: 6.5
- * WC tested up to: 8.2.2
+ * WC tested up to: 8.3.1
  *
  * @package Woocommerce_Product_Tabs
  */
+
+namespace Barn2\Plugin\WC_Product_Tabs_Free;
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Check if WooCommerce is active.
- */
-require_once ABSPATH . '/wp-admin/includes/plugin.php';
-
-if ( ! is_plugin_active( 'woocommerce/woocommerce.php' ) && ! function_exists( 'WC' ) ) {
-  return;
+// Prevent direct file access
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-// Bail if the WTA Pro is active
-if( is_plugin_active( 'woocommerce-product-tabs-pro/woocommerce-product-tabs.php' ) ) {
-	return;
-}
+const PLUGIN_VERSION = '2.1.0';
+const PLUGIN_FILE    = __FILE__;
 
-add_action( 'before_woocommerce_init', function() {
-	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
-		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
-	}
-} );
-
-// Define.
-define( 'WOOCOMMERCE_PRODUCT_TABS_NAME', 'Woocommerce Product Tabs' );
-define( 'WOOCOMMERCE_PRODUCT_TABS_SLUG', 'woocommerce-product-tabs' );
-define( 'WOOCOMMERCE_PRODUCT_TABS_VERSION', '2.0.23' );
-define( 'WOOCOMMERCE_PRODUCT_TABS_BASENAME', basename( dirname( __FILE__ ) ) );
-define( 'WOOCOMMERCE_PRODUCT_TABS_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) );
-define( 'WOOCOMMERCE_PRODUCT_TABS_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
-define( 'WOOCOMMERCE_PRODUCT_TABS_POST_TYPE_TAB', 'woo_product_tab' );
-define( 'WPT_UPGRADE_URL', 'https://barn2.com/wordpress-plugins/woocommerce-product-tabs/#pricing' );
-
-if ( ! defined( 'WP_WELCOME_DIR' ) ) {
-	define( 'WP_WELCOME_DIR', WOOCOMMERCE_PRODUCT_TABS_DIR . '/vendor/ernilambar/wp-welcome' );
-}
-
-if ( ! defined( 'WP_WELCOME_URL' ) ) {
-	define( 'WP_WELCOME_URL', WOOCOMMERCE_PRODUCT_TABS_URL . '/vendor/ernilambar/wp-welcome' );
-}
-
-// Include autoload.
-if ( file_exists( WOOCOMMERCE_PRODUCT_TABS_DIR . '/vendor/autoload.php' ) ) {
-	require_once WOOCOMMERCE_PRODUCT_TABS_DIR . '/vendor/autoload.php';
-	require_once WOOCOMMERCE_PRODUCT_TABS_DIR . '/vendor/ernilambar/wp-welcome/init.php';
-}
+// Autoloader.
+require_once __DIR__ . '/vendor/autoload.php';
 
 /**
- * The code that runs during plugin activation.
- */
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-woocommerce-product-tabs-activator.php';
-
-/**
- * The code that runs during plugin deactivation.
- */
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-woocommerce-product-tabs-deactivator.php';
-
-/** This action is documented in includes/class-woocommerce-product-tabs-activator.php */
-register_activation_hook( __FILE__, array( 'Woocommerce_Product_Tabs_Activator', 'activate' ) );
-
-/** This action is documented in includes/class-woocommerce-product-tabs-deactivator.php */
-register_deactivation_hook( __FILE__, array( 'Woocommerce_Product_Tabs_Deactivator', 'deactivate' ) );
-
-/**
- * The core plugin class that is used to define internationalization,
- * dashboard-specific hooks, and public-facing site hooks.
- */
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-woocommerce-product-tabs.php';
-
-// Load admin page.
-require_once plugin_dir_path( __FILE__ ) . 'admin/admin.php';
-
-/**
- * Begins execution of the plugin.
+ * Helper function to access the shared plugin instance.
  *
- * @since 1.0.0
+ * @return Plugin
  */
-function run_woocommerce_product_tabs() {
-	$plugin = new Woocommerce_Product_Tabs();
-	$plugin->run();
+function woocommerce_product_tabs() {
+	return Plugin_Factory::create( PLUGIN_FILE, PLUGIN_VERSION );
 }
 
-run_woocommerce_product_tabs();
+woocommerce_product_tabs()->register();
