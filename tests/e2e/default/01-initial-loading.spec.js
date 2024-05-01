d7866cf1a328b0e38baed3506ae9bbe6e69e179e
amirition <amirition@outlook.com>
2024-05-01 09:53:06 +0000

    added the initial loading tests

--- tests/e2e/default/01-initial-loading.spec.js
+++ tests/e2e/default/01-initial-loading.spec.js
@@ -4,17 +4,47 @@
 import {
 	test,
 	expect
-} from '@wordpress/e2e-test-utils-playwright';
+} from '../fixtures';
+
+import {
+	pluginCanBeActivated,
+	isWCDependencyNoticeVisible,
+	canAccessSetupWizard
+} from '@barn2plugins/playwright-utils';
+
+// Annotate the test suite as serial to ensure that the tests run in order.
+test.describe.configure({
+	mode: 'serial'
+});
 
 test.describe('initial loading', (props) => {
-	test('should have the "wpwrap" DOM element', async ({
+	test( 'should be able to activate the plugin', async ({
 		page,
 		admin,
+		pluginUtil
 	}, testInfo) => {
-		await admin.visitAdminPage('index.php');
+		const canActivate = await pluginCanBeActivated(page, admin, pluginUtil, true, expect);
+		expect(canActivate).toBe(true);
+	});
 
-		const wpwrap = await page.$('#wpwrap');
+	test( 'should display WooCommerce dependency notice', async ({
+		page,
+		admin,
+		pluginUtil
+	}, testInfo) => {
+		test.skip('Skipping test for now - needs debugging');
+		const displayedNotice = await isWCDependencyNoticeVisible(page, admin, pluginUtil, expect);
 
-		expect(wpwrap).toBeTruthy();
+		expect(displayedNotice).toBe(true);
+	});
+
+	test( 'can access Setup wizard page', async ({
+		page,
+		admin,
+		pluginUtil
+	}, testInfo) => {
+		const setupWizardIsAccessible = await canAccessSetupWizard(page, admin, pluginUtil, expect);
+
+		expect(setupWizardIsAccessible).toBe(true);
 	});
 });
